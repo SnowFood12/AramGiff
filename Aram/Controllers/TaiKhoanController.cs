cbc18a8acf9f29506f98140dc2f1db2cacc102fd
TUAN <hoangquoctuans3@gmail.com>
2023-12-06 19:58:58 +0700

    ddfdf

---
+++ Aram/Controllers/TaiKhoanController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aram.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DangKy()
+        {
+            return View();
+        }
+        public IActionResult DangNhap()
+        {
+            return View();
+        }
+        public IActionResult QuenMatKhau()
+        {
+            return View();
+        }
+        public IActionResult NhapOTP()
+        {
+            return View();
+        }
+        public IActionResult DoiMatKhauMoi()
+        {
+            return View();
+        }
+    }
+}
