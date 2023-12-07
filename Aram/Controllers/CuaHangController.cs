741d24d27377f8acc1cb5a30b808a6fd4b29c768
TUAN <hoangquoctuans3@gmail.com>
2023-12-07 23:20:07 +0700

    set default cho trang thai cua hang

--- Aram/Controllers/CuaHangController.cs
+++ Aram/Controllers/CuaHangController.cs
@@ -24,7 +24,7 @@ namespace Aram.Controllers
         public async Task<IActionResult> Index()
         {
               return _context.CuaHang != null ? 
-                          View(await _context.CuaHang.ToListAsync()) :
+                          View(await _context.CuaHang.Where(x => x.TrangThai == true).ToListAsync()) :
                           Problem("Entity set 'AramContext.CuaHang'  is null.");
         }
 
@@ -101,7 +101,7 @@ namespace Aram.Controllers
              if (ModelState.IsValid)
             {
                 cuaHang.NgayTaoCuaHang = DateTime.Now;
-                cuaHang.TenTK = "tuan1234";
+                cuaHang.TenTK = "admin";
                 _context.Add(cuaHang);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
