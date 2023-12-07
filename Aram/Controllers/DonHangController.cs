e29c7790077cbcb497f8ce901b91041033c6999f
lesykien <lesykien030@gmail.com>
2023-12-07 08:02:16 +0700

    thêm 2 giao diện quan lý đơn hàng cho admin

---
+++ Aram/Controllers/DonHangController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aram.Controllers
+{
+	public class DonHangController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Details()
+		{
+			return View();
+		}
+	}
+}
