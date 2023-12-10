f42c9a9d40d43bd29d0e3a9571cb735041407572
TUAN <hoangquoctuans3@gmail.com>
2023-12-10 18:51:42 +0700

    add component gio hang

---
+++ Aram/Components/GioHangWidget.cs
@@ -0,0 +1,38 @@
+﻿using Aram.Data;
+using Aram.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aram.Components
+{
+    public class GiohangWidget : ViewComponent
+    {
+        private readonly AramContext _context;
+
+        public GiohangWidget(AramContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var tenTK = HttpContext.Session.GetString("Name");
+            var gioHang = _context.GioHang.Where(p => p.TenTK == tenTK).FirstOrDefault();
+            int slItem = 0;
+            if (tenTK != null)
+            {
+                if (gioHang == null)
+                {
+                    gioHang = new GioHang { TenTK = tenTK };
+                    _context.GioHang.Add(gioHang);
+                    _context.SaveChanges();
+                }
+                var gioHang_chiTiet = _context.GioHang_ChiTiet
+                    .Include(p => p.SanPham)
+                    .Where(p => p.GioHangId == gioHang.Id).ToList();
+                slItem = gioHang_chiTiet.Count();
+
+            }
+            return View("Default", slItem);
+        }
+    }
+}
