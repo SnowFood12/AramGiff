a6979862f0378843b55319990313b4b94406d7c8
TUAN <hoangquoctuans3@gmail.com>
2023-12-13 13:24:21 +0700

    hh

--- Aram/Controllers/DonHangController.cs
+++ Aram/Controllers/DonHangController.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Aram.Data;
+using Aram.Infrastructure;
+using Aram.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Aram.Controllers
 {
 	public class DonHangController : Controller
 	{
-        public void PhanQuyen()
+		public GioHang? GioHang { get; set; }
+		private readonly AramContext _context;
+
+		public DonHangController(AramContext context)
+		{
+			_context = context;
+		}
+		public void PhanQuyen()
         {
             string Name = HttpContext.Session.GetString("Name");
             if (Name == "admin1234" && Name != null)
@@ -16,13 +26,40 @@ namespace Aram.Controllers
                 ViewBag.PhanQuyen = false;
             }
         }
-        public IActionResult Index()
+        public IActionResult Index() 
 		{
-			PhanQuyen();
 
             return View();
 		}
-		public IActionResult Details()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(string HoTen, string SoDT, string DiaChi, string GhiChu)
+        {
+            var donHang = new DonHang();
+			donHang.TenTK = HttpContext.Session.GetString("Name");
+			_context.Add(donHang);
+			_context.SaveChanges();
+			GioHang = HttpContext.Session.GetJson<GioHang>("giohang");
+			foreach (var item in GioHang.Lines)
+			{
+				var donHang_chiTiet = new DonHang_ChiTiet();
+				donHang_chiTiet.SanPhamId = item.SanPham.Id;
+				donHang_chiTiet.DonHangId = donHang.Id;
+				donHang_chiTiet.SoLuong = item.SoLuong;
+				_context.Add(donHang_chiTiet);
+				_context.SaveChanges();
+			}
+			var TT_NH = new ThongTin_NhanHang();
+			TT_NH.DonHangId = donHang.Id;
+			TT_NH.HoTen = HoTen;
+			TT_NH.SoDT = SoDT;
+			TT_NH.DiaChi = DiaChi;
+			TT_NH.GhiChu = GhiChu;
+			_context.Add(TT_NH);
+			_context.SaveChanges();
+			return RedirectToAction("DonHangDangGiao", "GioHang");
+        }
+        public IActionResult Details()
 		{
             return View();
 		}
